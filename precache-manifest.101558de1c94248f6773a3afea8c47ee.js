cb93bca29a4d2b3dfa85e5c247ef58996481ada4
siskaandrianit <siskaandriani1906@gmail.com>
2019-05-22 23:04:07 +0700

    Updates

---
+++ precache-manifest.101558de1c94248f6773a3afea8c47ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6295e63e935564ab7fa0eb23b5b12041",
+    "url": "/responsi2/index.html"
+  },
+  {
+    "revision": "2533d12054cacc389434",
+    "url": "/responsi2/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/responsi2/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "2533d12054cacc389434",
+    "url": "/responsi2/static/js/main.f3bbaa93.chunk.js"
+  },
+  {
+    "revision": "305675d0a646dfc47f78",
+    "url": "/responsi2/static/js/runtime~main.c495e7ef.js"
+  }
+]);
